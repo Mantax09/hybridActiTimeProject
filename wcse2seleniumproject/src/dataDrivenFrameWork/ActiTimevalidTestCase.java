66088c9798e62349c9c181c726babb62c45b1be2
Qspiders-Wakad <Qspiders-Wakad@DESKTOP-AIGVO6J>
2023-03-20 20:27:57 +0530

    commit

--- wcse2seleniumproject/src/dataDrivenFrameWork/ActiTimevalidTestCase.java
+++ wcse2seleniumproject/src/dataDrivenFrameWork/ActiTimevalidTestCase.java
@@ -24,11 +24,10 @@ public class ActiTimevalidTestCase {
 
 
 		Flib flib = new Flib();
-		String username = flib.readExcelData("./data/ActiTimeTestData.xlsx","validcreds",1, 0);
-		String password = flib.readExcelData("./data/ActiTimeTestData.xlsx","validcreds",1, 1);
+		
 		Thread.sleep(2000);
-		driver.findElement(By.name("username")).sendKeys(username);
-		driver.findElement(By.name("pwd")).sendKeys(password);
+		driver.findElement(By.name("username")).sendKeys(flib.readExcelData("./data/ActiTimeTestData.xlsx","validcreds",1, 0));
+		driver.findElement(By.name("pwd")).sendKeys(flib.readExcelData("./data/ActiTimeTestData.xlsx","validcreds",1, 1));
 		driver.findElement(By.id("loginButton")).click();
 
 
--- wcse2seleniumproject/src/dataDrivenFrameWork/Flib.java
+++ wcse2seleniumproject/src/dataDrivenFrameWork/Flib.java
@@ -15,6 +15,8 @@ public class Flib {
 	
 	// To Store Generic Reuseable Methods
 	
+	
+	// read the data from excel sheet
 	public String readExcelData(String excelPath,String sheetName,int rowCount,int cellCount) throws EncryptedDocumentException, IOException
 	{
 		FileInputStream fis = new FileInputStream(excelPath);//provide the path of file.
@@ -25,5 +27,32 @@ public class Flib {
 		String data = cell.getStringCellValue();// read the data from cell 
 		return data;
 	}
+	
+	
+	// to get row count
+	public int rowCount(String excelPath,String sheetName) throws EncryptedDocumentException, IOException
+	{
+		FileInputStream fis = new FileInputStream(excelPath);
+		Workbook wb = WorkbookFactory.create(fis);
+		Sheet sheet = wb.getSheet(sheetName);
+		
+		int rc = sheet.getLastRowNum();
+		return rc;
+	}
+	
+	
+	//to write data in excel sheet
+	public void writeExcelData(String excelPath,String sheetName,int rowCount,int cellCount,String data) throws EncryptedDocumentException, IOException
+	{
+		FileInputStream fis = new FileInputStream(excelPath);
+		Workbook wb = WorkbookFactory.create(fis);
+		Sheet sheet = wb.getSheet(sheetName);
+		Row row = sheet.getRow(rowCount);
+		
+		Cell cell = row.createCell(cellCount);
+		cell.setCellValue(data);
+	}
+	
+	
 
 }
